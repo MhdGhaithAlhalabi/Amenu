a2c72c02e60d4f2749831254e81403f399bec349
ghaithAlhalabi <78612789+ghaithAlhalabi@users.noreply.github.com>
2022-05-02 01:42:07 +0300

    add image

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -44,7 +44,7 @@ class ProductController extends Controller
             'type_id' => ['required'],
             'name' => ['required', 'string', 'max:255'],
             'details' => ['nullable'],
-            'image'=>'image|nullable|max:1999',
+            'image'=>'nullable',
             'price' => ['required'],
             'priceSale' => ['nullable'],
             'status' => ['nullable'],
@@ -55,27 +55,27 @@ class ProductController extends Controller
             return json_encode($validator->getMessageBag());
         }
         // Handle File Upload
-        if($request->hasFile('image'))
-        {
-            // Get filename with the extension
-            $filenameWithExt = $request->file('image')->getClientOriginalName();
-            // Get just filename
-            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-            // Get just ext
-            $extension = $request->file('image')->getClientOriginalExtension();
-            // Filename to store
-            $fileNameToStore= $filename.'_'.time().'.'.$extension;
-            // Upload Image
-            $path = $request->file('image')->storeAs('public/cover_images', $fileNameToStore);
-        }
-        else {
-            $fileNameToStore = NULL;
-        }
+//        if($request->hasFile('image'))
+//        {
+//            // Get filename with the extension
+//            $filenameWithExt = $request->file('image')->getClientOriginalName();
+//            // Get just filename
+//            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+//            // Get just ext
+//            $extension = $request->file('image')->getClientOriginalExtension();
+//            // Filename to store
+//            $fileNameToStore= $filename.'_'.time().'.'.$extension;
+//            // Upload Image
+//            $path = $request->file('image')->storeAs('public/cover_images', $fileNameToStore);
+//        }
+//        else {
+//            $fileNameToStore = NULL;
+//        }
         $product = Product::create([
             'type_id' => $request->type_id,
             'name' => $request->name,
             'details' => $request->details,
-            'image' => $fileNameToStore,
+            'image' => $request->image,
             'price' => $request->price,
             'priceSale' => $request->priceSale,
             'status' => $request->status,
@@ -121,7 +121,7 @@ class ProductController extends Controller
             'type_id' => ['required'],
             'name' => ['required', 'string', 'max:255'],
             'details' => ['nullable'],
-            'image'=>'image|nullable|max:1999',
+            'image'=>'nullable',
             'price' => ['required'],
             'priceSale' => ['nullable'],
             'status' => ['nullable'],
@@ -130,21 +130,21 @@ class ProductController extends Controller
         if ($validator->fails()) {
             return json_encode($validator->getMessageBag());
         }
-        // Handle File Upload
-        if($request->hasFile('image')){
-            // Get filename with the extension
-            $filenameWithExt = $request->file('image')->getClientOriginalName();
-            // Get just filename
-            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-            // Get just ext
-            $extension = $request->file('image')->getClientOriginalExtension();
-            // Filename to store
-            $fileNameToStore= $filename.'_'.time().'.'.$extension;
-            // Upload Image
-            $path = $request->file('image')->storeAs('public/cover_images', $fileNameToStore);
-        } else {
-            $fileNameToStore = NULL;
-        }
+//        // Handle File Upload
+//        if($request->hasFile('image')){
+//            // Get filename with the extension
+//            $filenameWithExt = $request->file('image')->getClientOriginalName();
+//            // Get just filename
+//            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+//            // Get just ext
+//            $extension = $request->file('image')->getClientOriginalExtension();
+//            // Filename to store
+//            $fileNameToStore= $filename.'_'.time().'.'.$extension;
+//            // Upload Image
+//            $path = $request->file('image')->storeAs('public/cover_images', $fileNameToStore);
+//        } else {
+//            $fileNameToStore = NULL;
+//        }
             $type_id =$request->type_id;
             $name =$request->name;
             $details =$request->details;
@@ -152,6 +152,7 @@ class ProductController extends Controller
             $priceSale = $request->priceSale;
             $status = $request->status;
             $time = $request->time;
+            $image = $request->image;
         $product->type_id = $type_id;
         $product->name = $name;
         $product->details =$details;
@@ -159,12 +160,13 @@ class ProductController extends Controller
         $product->priceSale = $priceSale;
         $product->status = $status;
         $product->time = $time;
-        if($request->hasFile('image')){
-            if($product->image != NULL) {
-                Storage::delete('public/cover_images/' . $product->image);
-                $product->image = $fileNameToStore;
-            }
-        }
+        $product->image = $image;
+//        if($request->hasFile('image')){
+//            if($product->image != NULL) {
+//                Storage::delete('public/cover_images/' . $product->image);
+//                $product->image = $fileNameToStore;
+//            }
+//        }
         $product->save();
 
         return json_encode('product edited');
@@ -179,10 +181,10 @@ class ProductController extends Controller
     public function destroy($id)
     {
         $product = Product::find($id);
-        if($product->image != NULL){
-            // Delete Image
-            Storage::delete('public/cover_images/'.$product->image);
-        }
+//        if($product->image != NULL){
+//            // Delete Image
+//            Storage::delete('public/cover_images/'.$product->image);
+//        }
         $product->delete();
         return json_encode('Product deleted');
     }
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 //});
 //basicAuth
 //auth:sanctum
-Route::middleware(['basicAuth'])->group(function (){
+Route::middleware(['auth:sanctum'])->group(function (){
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
