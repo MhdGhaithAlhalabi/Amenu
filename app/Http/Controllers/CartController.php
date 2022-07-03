37140f74788195bbead2071f034d9466b1f376ea
ghaithAlhalabi <78612789+ghaithAlhalabi@users.noreply.github.com>
2022-07-03 22:58:41 +0300

    order

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -49,7 +49,7 @@ class CartController extends Controller
             ->get();
         $carts = Cart::where('created_at','>' ,now()->subMonth())
             ->get();
-       $x= collect($carts1)->groupBy('created_at');
+       $x= collect($carts1)->groupBy('created_at','%d');
 
         $total = $carts->sum('amount');
         return ['report'=>$x,'total'=>$total ];
