ee0a78126c12a737aadc681e8c2f444421710c13
ghaithAlhalabi <78612789+ghaithAlhalabi@users.noreply.github.com>
2022-07-17 00:12:15 +0300

    f

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -335,10 +335,19 @@ class OrderController extends Controller
     }
 public function t(){
         try{
+         return   $user_info = DB::table('orders')
+                ->join('products', 'products.id', '=', 'orders.product_id')
+             ->select('products.name', DB::raw("to_date(cast(orders.created_at as text), 'YYYY MM DD') as date"),DB::raw('SUM(orders.qtu) AS sum'))
+                ->distinct()
+                ->whereDay('orders.created_at', '=',  Carbon::now()->month)
+                ->groupBy('date','products.name')
+                ->limit(3)
+                ->orderBy('sum', 'desc')
+                ->get();
     $product_id =  Product::all()->pluck('id');
     $purchases = DB::table('orders')
         ->join('products', 'products.id', '=', 'orders.product_id')
-        ->select('products.name', DB::raw("to_char(orders.created_at, 'DD') as date"),DB::raw('SUM(orders.qtu) AS sum'))
+        ->select('products.name', DB::raw("to_char(orders.created_at, 'HH24') as date"),DB::raw('SUM(orders.qtu) AS sum'))
         ->distinct()
         // ->where('orders.created_at',  Carbon::now()->month)
         ->whereDay('orders.created_at', '=',  Carbon::now()->day)
