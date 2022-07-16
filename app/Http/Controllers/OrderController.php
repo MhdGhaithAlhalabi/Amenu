0d20683ef21e6c34f936408b4577e9e428931b49
ghaithAlhalabi <78612789+ghaithAlhalabi@users.noreply.github.com>
2022-07-16 23:00:54 +0300

    f

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -286,7 +286,7 @@ class OrderController extends Controller
                $product_id =  Product::all()->pluck('id');
             $purchases = DB::table('orders')
                 ->join('products', 'products.id', '=', 'orders.product_id')
-             ->select('products.name', DB::raw("to_date(cast(orders.created_at as text), 'DD') as date"),DB::raw('SUM(orders.qtu) AS sum'))
+             ->select('products.name', DB::raw("to_date(cast(orders.created_at as text), 'YYYY MM DD') as date"),DB::raw('SUM(orders.qtu) AS sum'))
               ->distinct()
              // ->where('orders.created_at',  Carbon::now()->month)
              ->whereMonth('orders.created_at', '=',  Carbon::now()->month)
